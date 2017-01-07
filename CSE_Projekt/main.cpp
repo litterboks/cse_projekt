7a2619d8cab12e0d6f6ae7472083b7a5ef245546
Martin <motch1391@gmail.com>
2017-01-07 16:12:20 +0100

    removed files

--- CSE_Projekt/main.cpp
+++ CSE_Projekt/main.cpp
@@ -18,8 +18,8 @@ and may not be redistributed without written permission.*/
 #include <map>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 800;
 const int lod = LOW;
 
 //Starts up SDL, creates window, and initializes OpenGL
